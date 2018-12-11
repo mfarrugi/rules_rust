a4f944abab0b4df39bf3e74f8846580d90716566
Marco Farrugia <afmarco@gmail.com>
2018-12-11 09:11:59 -0500

    add another test function

--- examples/ffi/python_calling_rust/lib.rs
+++ examples/ffi/python_calling_rust/lib.rs
@@ -2,3 +2,8 @@
 pub extern fn my_favorite_number() -> i32 {
     4
 }
+
+#[no_mangle]
+pub extern fn triple_it(x: i32) -> i32 {
+    x * 3
+}
--- examples/ffi/python_calling_rust/main.py
+++ examples/ffi/python_calling_rust/main.py
@@ -7,6 +7,8 @@ class TestFFI(unittest.TestCase):
     def test_number(self):
         self.assertEquals(rusty.my_favorite_number(), 4)
 
+    def test_tripler(self):
+        self.assertEquals(rusty.triple_it(5), 15)
 
 if __name__ == "__main__":
     unittest.main()
